47aca5ba97172456e0655b3631195c197f6d429b
Ramy Tanios <ramy.tanios@gmail.com>
2025-11-08 10:01:40 +0100

    better gui

--- frontend/src/main/scala/rendezvous/View.scala
+++ frontend/src/main/scala/rendezvous/View.scala
@@ -18,7 +18,7 @@ trait View:
   val view =
     useState: state =>
       div(
-        cls := "bg-zinc-200 font-mono w-screen h-screen flex flex-col gap-2 items-center justify-center overflow-y-auto",
+        cls := "bg-zinc-100 font-mono w-screen h-screen flex flex-col gap-4 items-center justify-center overflow-y-auto",
         div(
           cls := "flex gap-2",
           sl.Button(
@@ -39,15 +39,16 @@ trait View:
           )
         ),
         div(
-          cls := "grid grid-cols-3 gap-4",
+          cls := "grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4",
           state.nodes.map: (nodeId, data) =>
             div(
-              cls := "relative",
+              cls := "relative h-48",
               sl.Card(
+                cls := "h-full",
                 sl.Card.slots.header := div(
-                  cls := "flex justify-between items-center gap-x-2",
+                  cls := "text-base flex justify-between items-center gap-x-2",
                   s"${truncateUUID(nodeId)}",
-                  sl.Icon(sl.Icon.name := "hdd-rack")
+                  sl.Icon(cls := "text-indigo-600", sl.Icon.name := "hdd-rack")
                 ),
                 sl.Card.slots.default := div(
                   cls := "flex flex-col items-start justify-center",
@@ -55,7 +56,7 @@ trait View:
                     val blinkCls = state.updates
                       .contains(Update(dataId, nodeId))
                       .valueOrZero(s"transition-color text-pink-500")
-                    span(cls := s"$blinkCls", s"${truncateUUID(dataId)}")
+                    span(cls := s"text-sm $blinkCls", s"${truncateUUID(dataId)}")
                   )
                 )
               ),
@@ -64,10 +65,13 @@ trait View:
               ),
               div(
                 cls := "absolute -top-3 -left-3",
-                sl.IconButton(
-                  sl.IconButton.name := "x-circle-fill",
-                  sl.IconButton.label := "RemoveNode",
-                  onClick := (_ => Some(Action.SendWS(dtos.WSProtocol.Client.RemoveNode(nodeId))))
+                sl.Tooltip(
+                  sl.Tooltip.content := "Stop node",
+                  sl.IconButton(
+                    sl.IconButton.name := "x-circle-fill",
+                    sl.IconButton.label := "RemoveNode",
+                    onClick := (_ => Some(Action.SendWS(dtos.WSProtocol.Client.RemoveNode(nodeId))))
+                  )
                 )
               )
             )
