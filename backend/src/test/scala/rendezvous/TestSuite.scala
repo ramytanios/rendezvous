d53596de4e42ef566c87b86465b9193fb9977750
Ramy Tanios <ramy.tanios@gmail.com>
2025-11-10 22:22:28 +0100

    more on unit tests

--- backend/src/test/scala/rendezvous/TestSuite.scala
+++ backend/src/test/scala/rendezvous/TestSuite.scala
@@ -2,6 +2,7 @@ package rendezvous.backend
 
 import munit.CatsEffectSuite
 import cats.effect.IO
+import mouse.all.*
 
 class TestSuite extends CatsEffectSuite:
 
@@ -9,5 +10,11 @@ class TestSuite extends CatsEffectSuite:
     Engine.resource().use: engine =>
       for
         nodeId <- engine.createNode
-        _ <- assertIO(engine.snapshot.map(_.keySet.size), 1)
+        _ <- assertIOBoolean:
+          engine.snapshot.map(_.keySet.size == 1)
+        dataId <- IO.randomUUID.flatTap(uuid => engine.addData(Data(uuid)))
+        _ <- assertIOBoolean:
+          engine.snapshot
+            .map(nodes => nodes.get(nodeId))
+            .existsF(_.snapshot.map(_.headOption).existsIn(_.id == dataId))
       yield ()
