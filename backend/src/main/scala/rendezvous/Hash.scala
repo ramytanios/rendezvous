aaa627fd0c991af188eceec15246c357e503299b
Ramy Tanios <ramy.tanios@gmail.com>
2025-11-13 09:59:03 +0100

    normalized hashes

--- backend/src/main/scala/rendezvous/Hash.scala
+++ backend/src/main/scala/rendezvous/Hash.scala
@@ -6,6 +6,10 @@ trait Hash:
 
   def hash(s: String): Int
 
+  final def normalized(s: String): Double = // in (0, 1)
+    val unsigned = hash(s) & 0xffffffffL
+    unsigned.toDouble / (1L << 32)
+
 object Hash:
 
   def apply(): Hash = mmh3()
